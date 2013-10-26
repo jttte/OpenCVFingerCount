659864965dbf22b58e868b698b0cb932c182c80a
Lucy Lin <Lucy@cardinalblue.com>
2013-10-26 22:13:45 +0800

    Initial Commit

---
+++ openCVtest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  openCVtest
+//
+//  Created by LucyLin on 2013/10/26.
+//  Copyright (c) 2013年 LucyLin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
